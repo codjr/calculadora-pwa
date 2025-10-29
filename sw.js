038e4766e29d9b17b6745feeebb237f4efe51609
Nelson Ferreira <nelson.pessoal.2022jr@gmail.com>
2025-10-29 11:04:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,28 @@
+const CACHE = 'calc-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './firebase-config.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res||fetch(e.request).then(net=>{
+      caches.open(CACHE).then(c=>c.put(e.request,net.clone()));
+      return net;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});

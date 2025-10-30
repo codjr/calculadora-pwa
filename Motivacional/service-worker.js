43ea41006978c84f94eda37ba88bed3628a80838
Nelson Ferreira <nelson.pessoal.2022jr@gmail.com>
2025-10-30 20:10:44 -0300

    Update service-worker.js

--- Motivacional/service-worker.js
+++ Motivacional/service-worker.js
@@ -1,34 +1,21 @@
-const CACHE="frases-pwa-v3";
+const CACHE="frases-pwa-v4";
 const ASSETS=[
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/imagem_icon_48x48.png",
-  "./icons/imagem_icon_72x72.png",
-  "./icons/imagem_icon_96x96.png",
-  "./icons/imagem_icon_128x128.png",
-  "./icons/imagem_icon_144x144.png",
-  "./icons/imagem_icon_152x152.png",
   "./icons/imagem_icon_192x192.png",
-  "./icons/imagem_icon_256x256.png",
-  "./icons/imagem_icon_384x384.png",
   "./icons/imagem_icon_512x512.png"
 ];
 
-// Instalação do cache
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// Busca do cache
 self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(resp=>resp||fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
 
-// Atualização de cache
 self.addEventListener("activate",e=>{
   e.waitUntil(
     caches.keys().then(keys=>
@@ -38,8 +25,32 @@ self.addEventListener("activate",e=>{
   self.clients.claim();
 });
 
-// Clique em notificação
 self.addEventListener("notificationclick",e=>{
   e.notification.close();
   e.waitUntil(clients.openWindow("./index.html"));
 });
+
+self.addEventListener("periodicsync",e=>{
+  if(e.tag==="frase-diaria"){
+    e.waitUntil(enviarFraseDiaria());
+  }
+});
+
+async function enviarFraseDiaria(){
+  const frases=[
+    "Acredite em você e vá além! 🚀",
+    "A disciplina vence o cansaço. 💪",
+    "Hoje é o dia perfeito para começar! 🌟",
+    "Você é capaz de mudar tudo! 🔥",
+    "Um passo de cada vez. 🧭",
+    "Persistência constrói resultados. 🏁",
+    "A ação cura a dúvida. 🎯",
+    "O impossível só existe até ser feito. ✨"
+  ];
+  const frase=frases[Math.floor(Math.random()*frases.length)];
+  await self.registration.showNotification("Frase do Dia 🌞",{
+    body:frase,
+    icon:"icons/imagem_icon_192x192.png",
+    badge:"icons/imagem_icon_96x96.png"
+  });
+}

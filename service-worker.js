380a1f0148106176a56cc604a696e7f8ac7a1c23
Nelson Ferreira <nelson.pessoal.2022jr@gmail.com>
2025-10-30 01:16:11 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,42 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("img-converter-v3").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.webmanifest",
-        "./icons/imagem_icon_192x192.png",
-        "./icons/imagem_icon_512x512.png"
-      ]);
+const CACHE_NAME = "conversor-pwa-v3";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/imagem_icon_192x192.png",
+  "./icons/imagem_icon_512x512.png"
+];
+
+// Instalação e cache inicial
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("📦 Cache inicial criado!");
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+// Ativação (limpa caches antigos)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  console.log("♻️ Service Worker ativo e limpo!");
+  return self.clients.claim();
+});
+
+// Fetch: responde com cache ou rede + fallback
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
   );
 });
